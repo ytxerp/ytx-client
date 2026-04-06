b53c7d6dd78874425602f6d68760f79c1b8156b7
YTX <ytx.cash@gmail.com>
2026-04-06 18:17:24 +0800

    Fix Finance Entry decimal places error

--- table/model/tablemodelf.cc
+++ table/model/tablemodelf.cc
@@ -122,8 +122,8 @@ bool TableModelF::UpdateNumeric(EntryShadow* shadow, double value, int row, bool
     QJsonObject update {};
     const bool is_parallel { shadow->is_parallel };
 
-    update.insert(is_parallel ? kLhsDebit : kRhsDebit, QString::number(*shadow->lhs_debit, 'f', NumericConst::kDecimalPlaces4));
-    update.insert(is_parallel ? kLhsCredit : kRhsCredit, QString::number(*shadow->lhs_credit, 'f', NumericConst::kDecimalPlaces4));
+    update.insert(is_parallel ? kLhsDebit : kRhsDebit, QString::number(*shadow->lhs_debit, 'f', NumericConst::kDecimalPlaces8));
+    update.insert(is_parallel ? kLhsCredit : kRhsCredit, QString::number(*shadow->lhs_credit, 'f', NumericConst::kDecimalPlaces8));
 
     QJsonObject message { JsonGen::EntryValue(section_, entry_id, update, is_parallel) };
 
