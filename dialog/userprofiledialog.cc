9dd1cf170385433b042cba39c4b161a805dcae9f
YTX <ytx.cash@gmail.com>
2026-05-11 12:17:41 +0800

    Refactor

--- dialog/userprofiledialog.cc
+++ dialog/userprofiledialog.cc
@@ -28,6 +28,8 @@ void UserProfileDialog::InitDialog()
     ui->lineEditUsername->setText(profile.Username());
     ui->lineEditName->setText(profile.Name());
 
+    ui->lineEditRole->setText(database_role::RoleDisplay(profile.GetDatabaseRole()));
+
     ui->pushButtonSave->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_S));
 }
 
--- workspace_member/databaserole.cc
+++ workspace_member/databaserole.cc
@@ -7,35 +7,33 @@ namespace database_role {
 std::span<const PermissionItem> RoleList()
 {
     static const PermissionItem list[] = {
-        { database_role::FINANCE_READONLY, QObject::tr("Finance R") },
-        { database_role::FINANCE_READWRITE, QObject::tr("Finance W") },
-        { database_role::TASK_READONLY, QObject::tr("Task R") },
-        { database_role::TASK_READWRITE, QObject::tr("Task W") },
-        { database_role::INVENTORY_READONLY, QObject::tr("Inventory R") },
-        { database_role::INVENTORY_READWRITE, QObject::tr("Inventory W") },
-        { database_role::PARTNER_READONLY, QObject::tr("Partner R") },
-        { database_role::PARTNER_READWRITE, QObject::tr("Partner W") },
-        { database_role::SALE_READONLY, QObject::tr("Sale R") },
-        { database_role::SALE_READWRITE, QObject::tr("Sale W") },
-        { database_role::PURCHASE_READONLY, QObject::tr("Purchase R") },
-        { database_role::PURCHASE_READWRITE, QObject::tr("Purchase W") },
+        { FINANCE_READONLY, QObject::tr("Finance R") },
+        { FINANCE_READWRITE, QObject::tr("Finance W") },
+        { TASK_READONLY, QObject::tr("Task R") },
+        { TASK_READWRITE, QObject::tr("Task W") },
+        { INVENTORY_READONLY, QObject::tr("Inventory R") },
+        { INVENTORY_READWRITE, QObject::tr("Inventory W") },
+        { PARTNER_READONLY, QObject::tr("Partner R") },
+        { PARTNER_READWRITE, QObject::tr("Partner W") },
+        { SALE_READONLY, QObject::tr("Sale R") },
+        { SALE_READWRITE, QObject::tr("Sale W") },
+        { PURCHASE_READONLY, QObject::tr("Purchase R") },
+        { PURCHASE_READWRITE, QObject::tr("Purchase W") },
     };
 
     return list;
 }
 
-QString RoleDisplay(database_role::PermissionBits bits)
+QString RoleDisplay(PermissionBits bits)
 {
-    QString result {};
+    QStringList result {};
 
     for (const auto& item : RoleList()) {
         if ((bits & item.bit) == item.bit) {
-            if (!result.isEmpty())
-                result += " | ";
-            result += item.text;
+            result.emplaceBack(item.text);
         }
     }
 
-    return result;
+    return result.join(" | ");
 }
 }
